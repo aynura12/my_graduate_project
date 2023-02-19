dedc4bc53fcbb1ae6943ad014bcce90527bee0bd
aynura12 <7m1bz3t@code.edu.az>
2023-02-19 02:18:01 -0800

    admin update and delete

--- fullstack/frontend/src/Context/ContextProvider.jsx
+++ fullstack/frontend/src/Context/ContextProvider.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react'
-
+import React, { useState ,useEffect} from 'react'
+import axios from 'axios';
 import { createContext } from "react";
 export const mainContext = createContext(null)
 const ContextProvider = ({children}) => {
-    const [state,setState]=useState([])
-
+    // const [state,setState]=useState([])
+    const [data, setData] = useState([]);
+    const getData = async () => {
+      const res = await axios.get("http://localhost:8080/gallery");
+      setData(res.data);
+    };
+    useEffect(() => {
+      getData();
+    }, []);
     const values = {
-       state,setState
+      data,setData,getData
     }
 
   return (
